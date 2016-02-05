0dba5ed6e4b940fc303663dac2cee8165783b8ca
MapleSun <maplesyj@gmail.com>
2016-02-05 11:19:25 -0800

    Initial Commit

---
+++ Compiler/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Compiler
+//
+//  Created by SYJ on 2/5/16.
+//  Copyright © 2016 SunYijie. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
